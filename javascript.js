ec655a6fea9d77fd0f8eebef109a7190b69ce4b8
hazamaswag <hamzasayyid@gmail.com>
2020-05-30 18:28:03 -0700

    test

--- javascript.js
+++ javascript.js
@@ -1,13 +1,15 @@
-var quotes = [
-  "Life isn’t about getting and having, it’s about giving and being.",
-  "Whatever the mind of man can conceive and believe, it can achieve.",
-  "Strive not to be a success, but rather to be of value.",
-  "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
-  "I attribute my success to this: I never gave or took any excuse.",
-  "You miss 100% of the shots you don’t take."
-]
+// var quotes = [
+//   "Life isn’t about getting and having, it’s about giving and being.",
+//   "Whatever the mind of man can conceive and believe, it can achieve.",
+//   "Strive not to be a success, but rather to be of value.",
+//   "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+//   "I attribute my success to this: I never gave or took any excuse.",
+//   "You miss 100% of the shots you don’t take."
+// ]
 
-// var quotes = JSON.parse('quotes.json');
+var fs = require('fs');
+var contents = fs.readFileSync('quotes.json');
+var quotes = JSON.parse('quotes.json', 'utf8');
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
