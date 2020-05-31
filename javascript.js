6261ffc05d8cf9c0c35b24a0ccb8e64195508ea2
hazamaswag <hamzasayyid@gmail.com>
2020-05-30 18:24:26 -0700

    test

--- javascript.js
+++ javascript.js
@@ -1,10 +1,13 @@
 var quotes = [
-  'Random Quotes',
-  'Haha Funny Kate!',
-  'More quotes'
+  "Life isn’t about getting and having, it’s about giving and being.",
+  "Whatever the mind of man can conceive and believe, it can achieve.",
+  "Strive not to be a success, but rather to be of value.",
+  "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.",
+  "I attribute my success to this: I never gave or took any excuse.",
+  "You miss 100% of the shots you don’t take."
 ]
 
-var quotes = JSON.parse('quotes.json');
+// var quotes = JSON.parse('quotes.json');
 
 function newQuote() {
   var randomNumber = Math.floor(Math.random() * (quotes.length));
